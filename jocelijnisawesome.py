db96e326bfb204338ea932cd5f42278075dd37d8
Jocelijn <jocelijn.cavero@gmail.com>
2016-06-19 22:02:32 +0200

    power

--- jocelijnisawesome.py
+++ jocelijnisawesome.py
@@ -1,6 +1,58 @@
 import urllib.request
 from html.parser import HTMLParser
 from pprint import pprint
+from math import sqrt
+
+class vector:
+    """
+    vector of length n
+    """
+
+    def __init__(self,n=0,L=[]):
+        self.n=n#length of vector
+        self.L=L#elements of vector
+
+    def __str__(self):
+        Str=[str(i) for i in self.L]
+        return str('\n'.join(Str))
+
+    def __add__(self,other):
+        return self.add_vector(other)
+
+    def __mul__(self,other):
+        if isinstance(other,vector):
+            return self.dot_product(other)
+        else:
+            return self.scalar(other)
+        
+    def scalar(self,other):
+        c=vector()
+        c.n=len(c.L)
+        c.L=[]
+        for i in range(len(self.L)):
+            c.L.append(str(self.L[i])+'*'+str(other))
+        return c
+
+    def add_vector(a,b):
+        c=vector()
+        c.n=len(c.L)
+        c.L=[]
+        for i in range(len(a.L)):
+            c.L.append(a.L[i]+b.L[i])
+        return c
+
+    def dot_product(a,b):
+        c=vector()
+        c.n=len(c.L)
+        c.L=[]
+        for i in range(len(a.L)):
+            c.L.append(int(a.L[i])*int(b.L[i]))
+        return sum(c.L)
+
+    def modulus(a):
+        c= sqrt(a.dot_product(a))
+        return c
+
 class matrix:
     """
     rows, columns, elements
@@ -20,6 +72,55 @@ class matrix:
             Str.append(STR)
             n+=1
         return str('\n'.join(Str))
+ 
+    def __mul__(self,other):#matrix times vector or matrix
+        if isinstance(other,matrix):
+            return self.mul_matrix(other)
+        elif isinstance(other,vector):
+            return self.mul_vector(other)
+
+    def mul_vector(self,other):
+        c=vector()
+        c.n=self.rows
+        c.L=[]
+        n=0
+        while n<self.rows:#rows of matrix as vectors
+            v=vector(len(self.elements[n]),self.elements[n])
+            c.L.append(v.dot_product(other))
+            #elements of new vector in terms of dot product
+            n+=1
+        return c
+
+    def mul_matrix(self,other):
+        c = matrix()
+        c.rows=self.rows
+        c.columns=other.columns
+        a=0
+        while a<c.columns:
+          lijst=[0 for a in range(c.columns)]
+          c.elements.append(lijst)
+          a+=1
+        i=0
+        while i<c.columns:
+            j=0
+            k=0
+            lst=[]
+            while j<c.columns:
+                lst.append(other.elements[j][i])
+                j+=1
+            while k<c.columns:
+                c.elements[k][i]=vector(self.rows,self.elements[k])*vector(self.rows,lst)
+                k+=1
+            i+=1
+        return c
+
+    def __pow__(self,other):
+        c=self
+        i=1
+        while i<int(other):
+            c=c.mul_matrix(self)
+            i+=1
+        return c
 
 # deze parser objecten zijn in staat om uit een HTML bestand alle
 # links te herkennen van het type <a href="{link}">text</a>
@@ -155,4 +256,7 @@ while x < len(dict.keys()):
     vertex=str(lijst[x])
     matrx.append(adj_matrix(lijst,dict,vertex))
     x=x+1
-#print(matrix(len(matrx[0]),len(matrx[0]),matrx))
+theone=matrix(len(matrx[0]),len(matrx[0]),matrx)
+
+print(theone)
+print(theone**5)
